61bbbb935d1439c39f7c9c276fc33b47f811e25c
helvisklei <helvisklei@hotmail.com>
2021-01-23 11:55:23 -0300

    conhecendo a estrutura do angular 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { MeuPrimeiroComponent } from './primeiro/meu-primeiro.componet';
+
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!</p>
-    `
+        <meu-primeiro-componente></meu-primeiro-componente>
+    `,
+    directives: [MeuPrimeiroComponent]
 })
 export class AppComponent { }
---
+++ app/primeiro/meu-primeiro.componet.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+    selector: 'meu-primeiro-componente',
+    template: '<h2> Meu primeiro componente Angular 2<h2>'
+
+})
+export class MeuPrimeiroComponent {
+
+   
+
+}
